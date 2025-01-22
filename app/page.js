42d53168da0b10ab6ff4e01aba2af86475ea7deb
Fabricio Lopez <fnla0911@gmail.com>
2025-01-21 21:05:34 -0300

    PrimeraEntregaNextJS

---
+++ app/page.js
@@ -0,0 +1,28 @@
+import Navbar from "./components/layouts/navbar/navbar";
+import Footer from "./components/layouts/footer/footer";
+//De forma estatica
+export const metadata = {
+  title: "Ecommerce | Home",
+  description: "Ecommerce",
+  keywords: "Ecommerce, tienda, online, shopping, Soluciones CRM Personalizadas",
+  openGraph: {
+    title: "Ecommerce | Home",
+    description: "Ecommerce para empresa",
+    url: "https://ecommerce.com",
+    siteName: "Ecommerce",
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9wZXxlbnwwfHwwfHx8MA%3D%3D",
+      },
+    ],
+  },
+};
+
+
+export default function Home() {
+  return (
+    <>
+      
+    </>
+  );
+}
